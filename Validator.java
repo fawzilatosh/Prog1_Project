a10c467d5b4dd055416bab667e3766df4061de49
Dustin Ruck <dustin.ruck@gmail.com>
2023-03-05 18:49:40 -0500

    Update Validator.java

--- Validator.java
+++ Validator.java
@@ -17,6 +17,62 @@ public class Validator {
     }
 		
 	
+//	public static boolean isPrefix(String prefix) {
+//    if (prefix.length() == 0) {
+//        return false;
+//    }
+//    if (!Character.isLetterOrDigit(prefix.charAt(0))) {
+//        return false;
+//    }
+//    for (int i = 1; i < prefix.length(); i++) {
+//        char c = prefix.charAt(i);
+//        if (!(Character.isLetterOrDigit(c) || c == '_' || c == '.' || c == '-')) {
+//            return false;
+//        }
+//        if ((c == '_' || c == '.' || c == '-') && (i == prefix.length() - 1 || !Character.isLetterOrDigit(prefix.charAt(i + 1)))) {
+//            return false;
+//        }
+//    }
+//    return true;
+//
+//
+//
+//public static boolean isDomain(String str) {
+//    String[] parts = str.split("\\.");
+//    if (parts.length != 2) {
+//        return false;
+//    }
+//    String firstPart = parts[0];
+//    String secondPart = parts[1];
+//    if (firstPart.isEmpty() || secondPart.isEmpty()) {
+//        return false;
+//    }
+//    for (int i = 0; i < firstPart.length(); i++) {
+//        char ch = firstPart.charAt(i);
+//        if (!Character.isLetterOrDigit(ch) && ch != '.' && ch != '-') {
+//            return false;
+//        }
+//        if ((ch == '.' || ch == '-') && (i == 0 || i == firstPart.length() - 1)) {
+//            return false;
+//        }
+//        if ((ch == '.' || ch == '-') && !Character.isLetterOrDigit(firstPart.charAt(i + 1))) {
+//            return false;
+//        }
+//    }
+//    for (int i = 0; i < secondPart.length(); i++) {
+//        char ch = secondPart.charAt(i);
+//        if (!Character.isLetter(ch)) {
+//            return false;
+//       }
+//    	}
+//    return true;
+//}
+
+
+
+
+
+}
 	
 	
 	
@@ -99,6 +155,19 @@ public class Validator {
 
         // if all checks passed, return lowercase input
         return input.toLowerCase();
+        
+     // public static boolean isSpecialChar(char c) {
+//      return c == '.' || c == '-' || c == '!';
+ // }
+
+ // public static boolean isAlphaNum(char c) {
+//      return Character.isLetterOrDigit(c);
+ // }
+
+ // public static boolean isPrefixChar(char c) {
+//      return c == '.' || c == '-';
+ // }
+        
     }
 
 
@@ -123,7 +192,7 @@ public class Validator {
 //	         }
 //	     }
 //	     return true;
-////   
+//   
 //	
 //	
 //	public static boolean isDomain(String str) {
@@ -159,17 +228,7 @@ public class Validator {
     
     
 
-// public static boolean isSpecialChar(char c) {
-//     return c == '.' || c == '-' || c == '!';
-// }
 
-// public static boolean isAlphaNum(char c) {
-//     return Character.isLetterOrDigit(c);
-// }
-
-// public static boolean isPrefixChar(char c) {
-//     return c == '.' || c == '-';
-// }
 
 
 
