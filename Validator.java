19bc4251ad529a09daa3457209de3f46596f4c8d
Dustin Ruck <dustin.ruck@gmail.com>
2023-03-05 23:51:38 -0500

    Update Validator.java

--- Validator.java
+++ Validator.java
@@ -13,69 +13,8 @@ public class Validator {
             }
         	}
         
-        
     }
 		
-	
-//	public static boolean isPrefix(String prefix) {
-//    if (prefix.length() == 0) {
-//        return false;
-//    }
-//    if (!Character.isLetterOrDigit(prefix.charAt(0))) {
-//        return false;
-//    }
-//    for (int i = 1; i < prefix.length(); i++) {
-//        char c = prefix.charAt(i);
-//        if (!(Character.isLetterOrDigit(c) || c == '_' || c == '.' || c == '-')) {
-//            return false;
-//        }
-//        if ((c == '_' || c == '.' || c == '-') && (i == prefix.length() - 1 || !Character.isLetterOrDigit(prefix.charAt(i + 1)))) {
-//            return false;
-//        }
-//    }
-//    return true;
-//
-//
-//
-//public static boolean isDomain(String str) {
-//    String[] parts = str.split("\\.");
-//    if (parts.length != 2) {
-//        return false;
-//    }
-//    String firstPart = parts[0];
-//    String secondPart = parts[1];
-//    if (firstPart.isEmpty() || secondPart.isEmpty()) {
-//        return false;
-//    }
-//    for (int i = 0; i < firstPart.length(); i++) {
-//        char ch = firstPart.charAt(i);
-//        if (!Character.isLetterOrDigit(ch) && ch != '.' && ch != '-') {
-//            return false;
-//        }
-//        if ((ch == '.' || ch == '-') && (i == 0 || i == firstPart.length() - 1)) {
-//            return false;
-//        }
-//        if ((ch == '.' || ch == '-') && !Character.isLetterOrDigit(firstPart.charAt(i + 1))) {
-//            return false;
-//        }
-//    }
-//    for (int i = 0; i < secondPart.length(); i++) {
-//        char ch = secondPart.charAt(i);
-//        if (!Character.isLetter(ch)) {
-//            return false;
-//       }
-//    	}
-//    return true;
-//}
-
-
-
-
-
-}
-	
-	
-	
 	// EMAIL
 	
     public static boolean isEmail(String email) {
@@ -155,20 +94,20 @@ public class Validator {
 
         // if all checks passed, return lowercase input
         return input.toLowerCase();
-        
-     // public static boolean isSpecialChar(char c) {
-//      return c == '.' || c == '-' || c == '!';
- // }
 
- // public static boolean isAlphaNum(char c) {
-//      return Character.isLetterOrDigit(c);
- // }
-
- // public static boolean isPrefixChar(char c) {
-//      return c == '.' || c == '-';
- // }
-        
+    public static boolean isSpecialChar(char c) {
+    return c == '.' || c == '-' || c == '!';
     }
+
+    public static boolean isAlphaNum(char c) {
+        return Character.isLetterOrDigit(c);
+    }
+
+    public static boolean isPrefixChar(char c) {
+        return c == '.' || c == '-';
+    }
+
+}
 
 
 
@@ -192,7 +131,7 @@ public class Validator {
 //	         }
 //	     }
 //	     return true;
-//   
+////   
 //	
 //	
 //	public static boolean isDomain(String str) {
@@ -228,7 +167,17 @@ public class Validator {
     
     
 
+// public static boolean isSpecialChar(char c) {
+//     return c == '.' || c == '-' || c == '!';
+// }
 
+// public static boolean isAlphaNum(char c) {
+//     return Character.isLetterOrDigit(c);
+// }
+
+// public static boolean isPrefixChar(char c) {
+//     return c == '.' || c == '-';
+// }
 
 
 
